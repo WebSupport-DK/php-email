f2b64ee939c6415becc7fa40d0213d65255c63ed
Thomas Elvin <thom855j@cvkweb.dk>
2015-09-10 10:03:53 +0200

    Update MailWrapper.php

--- MailWrapper.php
+++ MailWrapper.php
@@ -1,24 +1,7 @@
 <?php 
 
-/* 
- * Copyright (C) 2015 thoma 
- * 
- * This program is free software: you can redistribute it and/or modify 
- * it under the terms of the GNU General Public License as published by 
- * the Free Software Foundation, either version 3 of the License, or 
- * (at your option) any later version. 
- * 
- * This program is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
- * GNU General Public License for more details. 
- * 
- * You should have received a copy of the GNU General Public License 
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
- */ 
-
-namespace thom855j\PHPEmail ; 
-use thom855j\PHPEmail\Message;
+namespace thom855j\php_email ; 
+use thom855j\php_email\Message;
 
 class MailWrapper 
 { 
